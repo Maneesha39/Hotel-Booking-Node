491427e756ecafa5d48818d4a79a56b30f5347cb
venkateswarlu <nallapadu.venkateswarlu@gmail.com>
2019-04-09 19:49:05 +0530

    created view to display data from 2 tables

--- Backend/Services/hotelService.js
+++ Backend/Services/hotelService.js
@@ -1,7 +1,8 @@
 const knex = require('../db');
 
 exports.getHotels = async () => {
-    const result = await knex.select('*').from("hotel_list")
+    // const result = await knex.select('*').from("hotel_list")
+    const result = await knex.select('*').from('hotel_address');
     console.log(result);
     return result;
 }
