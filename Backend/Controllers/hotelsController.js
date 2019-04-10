2c85b4f5452cee6dc2b3477a5fcf58c3f1a65c66
venkateswarlu <nallapadu.venkateswarlu@gmail.com>
2019-04-10 19:35:40 +0530

    add hotel post method

--- Backend/Controllers/hotelsController.js
+++ Backend/Controllers/hotelsController.js
@@ -7,4 +7,16 @@ router.get('/', async (req, res) => {
     res.json({ hotels: hotelsList })
 })
 
+router.post('/', async (req, res) => {
+    try {
+        console.log("I'm in hotels")
+        await hotelService.insert(req.body)
+        res.json({ message: "Inserted hotels Successfully" })
+    }
+    catch (err) {
+        console.log(err);
+        res.status(422).json({ message: err })
+    }
+})
+
 module.exports = router
--- Backend/Services/hotelService.js
+++ Backend/Services/hotelService.js
@@ -2,7 +2,18 @@ const knex = require('../db');
 
 exports.getHotels = async () => {
     // const result = await knex.select('*').from("hotel_list")
-    const result = await knex.select('*').from('hotel_address');
+    const result = await knex.select('*').from('hotel_list');
     console.log(result);
     return result;
+}
+
+exports.insert = async (hotels) => {
+    try {
+        const hotelsData = JSON.parse(JSON.stringify(hotels))
+        await knex.insert(hotelsData).table('hotel_list')
+    }
+    catch (err) {
+        console.log(err)
+        throw 'Unable to insert hotels'
+    }
 }
